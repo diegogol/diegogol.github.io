01b4719ab60f80bd31172cea22995af8ee40cf9f
diegogol <dicheck_km13@hotmail.com>
2017-02-21 14:22:02 -0600

    Update Egeometry21.02.js

--- Egeometry21.02.js
+++ Egeometry21.02.js
@@ -5,6 +5,13 @@ forma.vertices.push(new THREE.Vector3(-1,0,-1));
 forma.vertices.push(new THREE.Vector3(-1,0,1));
 forma.vertices.push(new THREE.Vector3(0,1,0));
 
+forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
+forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
+forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
+forma.faces.push( new THREE.Face3( 0, 1, 4 ) ); // Cara 3
+forma.faces.push( new THREE.Face3( 1, 2, 4 ) ); // Cara 4
+forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 5
+
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
 
