0bba4879ca5d8dc23ae31d742761307933d66ee5
diegogol <dicheck_km13@hotmail.com>
2017-02-23 14:05:16 -0600

    Update

--- malla.js
+++ malla.js
@@ -8,15 +8,15 @@ var forma = new THREE.ExtrudeGeometry( figura,
                                        {amount: 100 );
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-malla.rotateY( Math.PI/8 );
+malla.rotateY( Math.PI/4 );
 var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 100;
+camara.position.z = 500;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.50,
-                      window.innerHeight*.50);
+renderizador.setSize( window.innerHeight*.950,
+                      window.innerHeight*.950);
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
