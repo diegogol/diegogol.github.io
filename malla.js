2f0c627d11f9fa6e36874b4d728781e6b9380fdd
diegogol <dicheck_km13@hotmail.com>
2017-02-28 13:24:10 -0600

    Update malla.js

--- malla.js
+++ malla.js
@@ -5,7 +5,7 @@ figura.lineTo(10, 40);
 figura.lineTo(40, 40);
 figura.lineTo(10, 10);
 var forma = new THREE.ExtrudeGeometry( figura,
-                                       {amount: 10);
+                                       {amount: 10});
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
 malla.rotateY( Math.PI/4 );
