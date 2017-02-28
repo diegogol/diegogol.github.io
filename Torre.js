e3bfa93dbb950898b6b3c6c43d504fe9481fbed9
diegogol <dicheck_km13@hotmail.com>
2017-02-28 14:25:27 -0600

    Update Torre.js

--- Torre.js
+++ Torre.js
@@ -1,15 +1,23 @@
-var figura = new THREE.Shape();
+var puntos = [];
+for ( var i = 0; i < 40; i ++ ) {
+  if (i<15){
+    puntos.push( new THREE.Vector2( i,40);    
+  }
+   if (i>15&&i<30){
+    puntos.push( new THREE.Vector2( 15,55-i);    
+  }
 
-figura.moveTo(10, 10);
-figura.lineTo(10, 40);
-figura.lineTo(40, 40);
-figura.lineTo(40, 10);
-figura.lineTo(10, 10);
-var forma = new THREE.ExtrudeGeometry( figura,
-                                       {amount: 10});
+   if (i>30&&i<45){
+    puntos.push( new THREE.Vector2( 45-i,25);    
+  }
+                
+}
+var forma = new THREE.LatheGeometry(puntos);
+
 var material = new THREE.MeshNormalMaterial();
+
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX( Math.PI/2 );
+malla.rotateX( Math.PI/6 );
 var escena = new THREE.Scene();
 escena.add(malla);
 
@@ -17,7 +25,6 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z = 500;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95,
-                      window.innerHeight*.95);
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
