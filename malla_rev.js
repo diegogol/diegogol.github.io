9d184392734c9925df2c47834ed58a47c4a1b3ea
diegogol <dicheck_km13@hotmail.com>
2017-02-28 14:03:22 -0600

    Update malla_rev.js

--- malla_rev.js
+++ malla_rev.js
@@ -1,8 +1,8 @@
 var puntos = [];
 for ( var i = 0; i < 80; i ++ ) {
     puntos.push( new THREE.Vector2(
-                     Math.sin( i * 0.2 ) * 15 + 50,
-                     ( i - 5 ) * 2 ) );
+                     Math.sin( i * 0.4 ) * 15 + 50,
+                     ( i - 5 ) * 3) );
 }
 var forma = new THREE.LatheGeometry(puntos);
 
