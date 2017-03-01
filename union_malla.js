ecdbc4bdfb7d3a748b3435316c926b2368b011d4
diegogol <dicheck_km13@hotmail.com>
2017-03-01 12:57:56 -0600

    Update union_malla.js

--- union_malla.js
+++ union_malla.js
@@ -1,6 +1,6 @@
 var troncoForma = new THREE.CylinderGeometry(.25, .5, 1);
 var esferaForma = new THREE.SphereGeometry(.65);
-esferaForma.translate(0,1,0);
+esferaForma.translate(1,0,0);
 var troncoMalla = new THREE.Mesh(troncoForma);
 var esferaMalla = new THREE.Mesh(esferaForma);
 var arbolForma = new THREE.Geometry();
