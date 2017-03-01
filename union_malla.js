ce85073b62a91afe5c5b4488c2fa707eac31f1ca
diegogol <dicheck_km13@hotmail.com>
2017-03-01 13:06:16 -0600

    Update union_malla.js

--- union_malla.js
+++ union_malla.js
@@ -13,7 +13,7 @@ var escena = new THREE.Scene();
 escena.add(arbolMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 50;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
