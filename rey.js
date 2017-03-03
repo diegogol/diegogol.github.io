eb891ee3bb16ba969388dfd7f3cc694c133d4fd5
diegogol <dicheck_km13@hotmail.com>
2017-03-03 02:34:09 -0600

    Update rey.js

--- rey.js
+++ rey.js
@@ -1,17 +1,21 @@
-var troncoForma = new THREE.CylinderGeometry(.8, .5, 1);
-var troncoForma2 = new THREE.CylinderGeometry(1, 1, 1);
-//var troncoForma3= new THREE.CylinderGeometry(.5, .5, 1);
+var troncoForma = new THREE.CylinderGeometry(.8, .5, 1.5);
+var troncoForma2 = new THREE.CylinderGeometry(1, 1, .5);
+var troncoForma3= new THREE.CylinderGeometry(.4, .8, 1);
 //var troncoForma4= new THREE.CylinderGeometry(.5, .5, 1);
 var esferaForma = new THREE.SphereGeometry(.5);
 esferaForma.translate(0,1,0);
-troncoForma2.translate(0,-.5,0);
+troncoForma2.translate(0,-1,0);
+troncoForma3.translate(0,-2.5,0);
+
 var troncoMalla = new THREE.Mesh(troncoForma);
 var troncoMalla2 = new THREE.Mesh(troncoForma2);
+var troncoMalla3 = new THREE.Mesh(troncoForma3);
 var esferaMalla = new THREE.Mesh(esferaForma);
 var arbolForma = new THREE.Geometry();
 arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
 arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
 arbolForma.merge(troncoMalla2.geometry, troncoMalla2.matrix);
+arbolForma.merge(troncoMalla3.geometry, troncoMalla3.matrix);
 
 var material = new THREE.MeshNormalMaterial();
 var arbolMalla = new THREE.Mesh(arbolForma, material);
