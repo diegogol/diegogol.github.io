467fcb3e1826ac96e1c83b4c7f185e55286d6503
diegogol <dicheck_km13@hotmail.com>
2017-03-03 00:07:10 -0600

    Update torre.js

--- torre.js
+++ torre.js
@@ -22,13 +22,13 @@ var forma = new THREE.LatheGeometry(puntos);
 var material = new THREE.MeshNormalMaterial();
       
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX( Math.PI/5);
+malla.rotateX( Math.PI/6);
       
 var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 500;
+camara.position.z = 50;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
