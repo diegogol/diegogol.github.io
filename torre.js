770b5ee1d6086f173efb159cbb36c85a94ec7d2d
diegogol <dicheck_km13@hotmail.com>
2017-03-03 00:52:56 -0600

    Update torre.js

--- torre.js
+++ torre.js
@@ -35,7 +35,7 @@ var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 100;
+camara.position.z = 150;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
