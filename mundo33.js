845544ccc6fbdd99be63c501599da7cd8ed4594b
diegogol <dicheck_km13@hotmail.com>
2017-03-03 22:51:31 -0600

    Update and rename mundo32.js to mundo33.js

--- mundo33.js
+++ mundo33.js
@@ -1,3 +1,4 @@
+//bando 1 Amarillo
 //Rey
 var troncoForma = new THREE.CylinderGeometry(18, 11, 24);
 var troncoForma2 = new THREE.CylinderGeometry(17, 17, 12);
@@ -104,15 +105,106 @@ var materialCa = new THREE.MeshBasicMaterial( {color: 0xffff00} );
 arbolFormaCa.translate(90,0,0);
 var arbolmallacab = new THREE.Mesh(arbolFormaCa, materialCa);
 
+//bando 2 Verde
+//Rey
+var troncoForma = new THREE.CylinderGeometry(18, 11, 24);
+var troncoForma2 = new THREE.CylinderGeometry(17, 17, 12);
+var troncoForma3= new THREE.CylinderGeometry(8, 11, 22);
+var troncoForma4= new THREE.CylinderGeometry(16, 12, 17);
+var esferaForma = new THREE.SphereGeometry(12);
+esferaForma.translate(0,70,0);
+troncoForma.translate(0,50,0);
+troncoForma2.translate(0,30,0);
+troncoForma3.translate(0,20,0);
+troncoForma4.translate(0,0,0);
+
+var troncoMalla = new THREE.Mesh(troncoForma);
+var troncoMalla2 = new THREE.Mesh(troncoForma2);
+var troncoMalla3 = new THREE.Mesh(troncoForma3);
+var troncoMalla4 = new THREE.Mesh(troncoForma4);
+var esferaMalla = new THREE.Mesh(esferaForma);
+var arbolForma2 = new THREE.Geometry();
+arbolForma2.merge(troncoMalla.geometry, troncoMalla.matrix);
+arbolForma2.merge(esferaMalla.geometry, esferaMalla.matrix);
+arbolForma2.merge(troncoMalla2.geometry, troncoMalla2.matrix);
+arbolForma2.merge(troncoMalla3.geometry, troncoMalla3.matrix);
+arbolForma2.merge(troncoMalla4.geometry, troncoMalla4.matrix);
+
+var materialRey2 = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+arbolForma2.translate(0,0,-20);
+var arbolMalla2 = new THREE.Mesh(arbolForma2, materialRey2);
+
+//torre
+var puntos2 = [];
+for (var i=0; i<76; i++) {
+  if (i<15){  puntos2.push( new THREE.Vector2( i,40));}
+  if (i>15&&i<30){  puntos2.push( new THREE.Vector2( 15,56-i));}
+  if (i>30&&i<40){  puntos2.push( new THREE.Vector2( 45-i,25));}
+  if (i>40&&i<60){  puntos2.push( new THREE.Vector2(-41+i,70-i));}
+  if (i>60&&i<65){  puntos2.push( new THREE.Vector2(-41+i,10));}
+  if (i>65&&i<75){  puntos2.push( new THREE.Vector2(24,76-i));}
+  if (i>75&&i<85){  puntos2.push( new THREE.Vector2(85-i,0));}             
+}              
+var formaT2 = new THREE.LatheGeometry(puntos2);
+var materialT2 = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+formaT2.translate(60,-10,-20);
+var mallaT2 = new THREE.Mesh( formaT2, materialT2);
+
+//Caballo
+var figuraCa2 = new THREE.Shape();
+
+figuraCa2.moveTo(10, 5);
+figuraCa2.lineTo(2, 20);
+figuraCa2.lineTo(10, 20);
+figuraCa2.lineTo(10, 25);
+figuraCa2.lineTo(5, 28);
+figuraCa2.lineTo(0, 28);
+figuraCa2.lineTo(-3, 30);
+figuraCa2.lineTo(-7, 27);
+figuraCa2.lineTo(-10, 20);
+figuraCa2.lineTo(-10, 15);
+figuraCa2.lineTo(-7, 15);
+figuraCa2.lineTo(-7, 5);
+figuraCa2.lineTo(10, 5);
+
+var base2 = new THREE.CylinderGeometry(20, 20, 5);
+base2.translate(0,0,0);
+var mallabase2 = new THREE.Mesh(base2);
+
+var formaCa2 = new THREE.ExtrudeGeometry( figuraCa2,{amount: 1});
+var mallaCa2 = new THREE.Mesh(formaCa2);
+
+var arbolFormaCa2 = new THREE.Geometry();
+arbolFormaCa2.merge(mallabase2.geometry, mallabase2.matrix);
+arbolFormaCa2.merge(mallaCa2.geometry, mallaCa2.matrix);
+
+var materialCa2 = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+arbolFormaCa2.translate(90,0,0);
+var arbolmallacab2 = new THREE.Mesh(arbolFormaCa2, materialCa2);
+
+//
+
+
 var escena = new THREE.Scene();
+//bando 1 amarillo
 escena.add(arbolMalla);
 escena.add(mallaT);
 escena.add(mallaTabl);
 escena.add(arbolmallacab);
+//bando 2 verde
+escena.add(arbolMalla2);
+escena.add(mallaT2);
+escena.add(arbolmallacab2);
+//bando 1 Amarillo
 arbolForma.rotateX( Math.PI/4 );
 formaT.rotateX( Math.PI/4 );
 formaTabl.rotateX( Math.PI/4 );
 arbolFormaCa.rotateX( Math.PI/4 );
+//bando 2 verde
+arbolForma2.rotateX( Math.PI/4 );
+formaT2.rotateX( Math.PI/4 );
+arbolFormaCa2.rotateX( Math.PI/4 );
+
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 350;
 
