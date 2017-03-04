575d67b167a4b623b8138fdeb76034acb5879d6c
diegogol <dicheck_km13@hotmail.com>
2017-03-03 21:30:25 -0600

    Rename caballo.js to caballo1.js

---
+++ caballo1.js
@@ -0,0 +1,32 @@
+var figura = new THREE.Shape();
+
+figura.moveTo(10, 5);
+figura.lineTo(2, 20);
+figura.lineTo(10, 20);
+figura.lineTo(10, 25);
+figura.lineTo(5, 28);
+figura.lineTo(0, 28);
+figura.lineTo(-3, 30);
+figura.lineTo(-7, 27);
+figura.lineTo(-10, 20);
+figura.lineTo(-10, 15);
+figura.lineTo(-7, 15);
+figura.lineTo(-7, 5);
+figura.lineTo(10, 5);
+
+
+var forma = new THREE.ExtrudeGeometry( figura,{amount: 1});
+var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh( forma, material );
+malla.rotateY( Math.PI/4 );
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 500;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95,
+                      window.innerHeight*.95);
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
