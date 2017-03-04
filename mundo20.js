987ba6b54864c3dbdd7c97174fd5718c06e17f89
diegogol <dicheck_km13@hotmail.com>
2017-03-03 22:04:35 -0600

    Update and rename mundo19.js to mundo20.js

--- mundo20.js
+++ mundo20.js
@@ -72,10 +72,43 @@ formaTabl.computeFaceNormals();
 var materialTabl = new THREE.MeshNormalMaterial();
 var mallaTabl= new THREE.Mesh( formaTabl, materialTabl );
 
+//Caballo
+var figuraCa = new THREE.Shape();
+
+figuraCa.moveTo(10, 5);
+figuraCa.lineTo(2, 20);
+figuraCa.lineTo(10, 20);
+figuraCa.lineTo(10, 25);
+figuraCa.lineTo(5, 28);
+figuraCa.lineTo(0, 28);
+figuraCa.lineTo(-3, 30);
+figuraCa.lineTo(-7, 27);
+figuraCa.lineTo(-10, 20);
+figuraCa.lineTo(-10, 15);
+figuraCa.lineTo(-7, 15);
+figuraCa.lineTo(-7, 5);
+figuraCa.lineTo(10, 5);
+
+var base = new THREE.CylinderGeometry(20, 20, 5);
+base.translate(0,0,0);
+var mallabase = new THREE.Mesh(base);
+
+var formaCa = new THREE.ExtrudeGeometry( figuraCa,{amount: 1});
+var mallaCa = new THREE.Mesh(formaCa);
+
+var arbolFormaCa = new THREE.Geometry();
+arbolFormaCa.merge(mallabase.geometry, mallabase.matrix);
+arbolFormaCa.merge(mallaCa.geometry, mallaCa.matrix);
+
+var materialCa= new THREE.MeshNormalMaterial();
+arbolFormaCa.translate(80,0,0);
+var arbolmallacab = new THREE.Mesh(arbolFormaCa, materialCa);
+
 var escena = new THREE.Scene();
 escena.add(arbolMalla);
 escena.add(mallaT);
 escena.add(mallaTabl);
+escena.add(arbolmallaca);
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 350;
