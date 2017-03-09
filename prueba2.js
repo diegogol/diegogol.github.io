6991ad74899d94c7a976d1e2496d6104c7c16ef3
diegogol <dicheck_km13@hotmail.com>
2017-03-09 13:33:06 -0600

    Update and rename prueba1.js to prueba2.js

--- prueba2.js
+++ prueba2.js
@@ -9,17 +9,22 @@ camara.near = 0.1;
 camara.far = 1000;
 camara.updateProjectionMatrix();
 
-camara.position.z = 100;
+camara.position.z = 30;
+
 for (var i=0;i<3;i++)
 {
-var cubo = new THREE.Mesh( new THREE.BoxGeometry(10,10,10), new THREE.MeshNormalMaterial());
-cubo.position.x =30*i;
+var cubo[i] = new THREE.Mesh( new THREE.BoxGeometry(10,10,10), new THREE.MeshNormalMaterial());
+  cubo[i].position.x =30*i;
 
 }
 
 var escena = new THREE.Scene();
 
-escena.add(cubo);
+escena.add(cubo(0));
+
+escena.add(cubo(1));
+
+escena.add(cubo(2));
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth, window.innerHeight);
