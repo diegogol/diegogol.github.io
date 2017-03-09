63464b3ebd8a55661e00186550ee00b0c270e4ca
diegogol <dicheck_km13@hotmail.com>
2017-03-09 13:35:36 -0600

    Update and rename prueba2.js to prueba3.js

--- prueba3.js
+++ prueba3.js
@@ -13,8 +13,8 @@ camara.position.z = 30;
 
 for (var i=0;i<3;i++)
 {
-var cubo[i] = new THREE.Mesh( new THREE.BoxGeometry(10,10,10), new THREE.MeshNormalMaterial());
-  cubo[i].position.x =30*i;
+var cubo(i) = new THREE.Mesh( new THREE.BoxGeometry(10,10,10), new THREE.MeshNormalMaterial());
+  cubo(i).position.x =30*i;
 
 }
 
