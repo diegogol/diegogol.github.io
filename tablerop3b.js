bd4afe6327e3d5ae5ca81fc8783db5074d1a9483
diegogol <dicheck_km13@hotmail.com>
2017-03-20 19:55:52 -0600

    Update and rename tablerop2a.js to tablerop3b.js

--- tablerop3b.js
+++ tablerop3b.js
@@ -6,10 +6,10 @@ camara.right = window.innerWidth/2;
 camara.top = window.innerHeight/2;
 camara.bottom= window.innerHeight/-2;
 camara.near = 0.1;
-camara.far = 100;
+camara.far = 1000;
 camara.updateProjectionMatrix();
 
-camara.position.y = 10;
+camara.position.y = 100;
 var escena = new THREE.Scene();
 var cubo;
 for (var i=0;i<8;i++)
