8e930df98c5e817ebec43efd266c9fc9a0bfd498
diegogol <dicheck_km13@hotmail.com>
2017-03-20 20:37:30 -0600

    Update and rename tablerop8g.js to tablerop9g.js

--- tablerop9g.js
+++ tablerop9g.js
@@ -4,7 +4,7 @@ camara.left = window.innerWidth/-2;
 camara.right = window.innerWidth/2;
 camara.top = window.innerHeight/2;
 camara.bottom= window.innerHeight/-2;
-camara.near = 0.1;
+camara.near = 0.5;
 camara.far = 100;
 camara.updateProjectionMatrix();
 
