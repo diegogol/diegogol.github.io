f5480be30021acf94583f301ef638ed617c25f35
diegogol <dicheck_km13@hotmail.com>
2017-03-20 20:53:58 -0600

    Update and rename tablerop1q.js to tablerop2k.js

--- tablerop2k.js
+++ tablerop2k.js
@@ -5,7 +5,7 @@ camara.right = window.innerWidth/3;
 camara.top = window.innerHeight/3;
 camara.bottom= window.innerHeight/-3;
 camara.near = 1;
-camara.far = 30;
+camara.far = 40;
 camara.updateProjectionMatrix();
 
 camara.position.z = 50;
