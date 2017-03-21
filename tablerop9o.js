9da65482a13f30508c5c1402df00a38d7de088b9
diegogol <dicheck_km13@hotmail.com>
2017-03-20 21:12:13 -0600

    Update and rename tablerop6y.js to tablerop9o.js

---
+++ tablerop9o.js
@@ -0,0 +1,79 @@
+var camara = new THREE.OrthographicCamera();
+
+camara.left = window.innerWidth/-5;
+camara.right = window.innerWidth/5;
+camara.top = window.innerHeight/5;
+camara.bottom= window.innerHeight/-5;
+camara.near = 1;
+camara.far = 45;
+camara.updateProjectionMatrix();
+
+camara.position.z = 30;
+//Tablero multicolor
+var escena = new THREE.Scene();
+var cubo;
+for (var i=0;i<8;i++)
+{
+for (var j=0;j<8;j++)
+{
+var cubo= new THREE.Mesh( new THREE.BoxGeometry(10,10,10));
+  cubo.position.x =j*10;
+  cubo.position.y=i*10;
+  escena.add(cubo);
+}
+}
+//CRUZ
+var puntos2 = [];
+for ( var i = 0; i <=9; i ++ ) {
+if(i<=1){     puntos2.push( new THREE.Vector2(i,16));}
+if(i==2){    puntos2.push( new THREE.Vector2(1,18));}
+if(i==3){    puntos2.push( new THREE.Vector2(3,18));}
+if(i==4){    puntos2.push( new THREE.Vector2(3,20));}
+if(i==5){    puntos2.push( new THREE.Vector2(1,20));}
+if(i==6){   puntos2.push( new THREE.Vector2(1,22));}
+    if(i==7){    puntos2.push( new THREE.Vector2(0,22));}
+      if(i==8){    puntos2.push( new THREE.Vector2(0,16));}}
+var cruz = new THREE.LatheGeometry(puntos2);
+
+//CUERPO
+var puntos = [];
+for ( var i = 0; i <=9; i ++ ) {
+if(i<=4){     puntos.push( new THREE.Vector2(i,16));}
+if(i==5){     puntos.push( new THREE.Vector2(2,11));}
+if(i==6){     puntos.push( new THREE.Vector2(4,9));}
+if(i==7){     puntos.push( new THREE.Vector2(4,8));}
+if(i==8){    puntos.push( new THREE.Vector2(2,8));}
+if(i==9){     puntos.push( new THREE.Vector2(4,0));}
+    if(i==10){    puntos.push( new THREE.Vector2(0,0));}}
+var cuerpo = new THREE.LatheGeometry(puntos);
+
+//BASE
+var puntos1 = [];
+for ( var i = 0; i <=22; i ++ ) {
+if(i<=6){     puntos1.push( new THREE.Vector2(i,0));}
+if(i==7){     puntos1.push( new THREE.Vector2(6,-2));}
+if(i==8){    puntos1.push( new THREE.Vector2(0,-2));}}
+var base = new THREE.LatheGeometry(puntos1);
+
+//UNION
+var cruzMalla = new THREE.Mesh(cruz);
+var cuerpoMalla = new THREE.Mesh(cuerpo);
+var baseMalla = new THREE.Mesh(base);
+
+var reyForma= new THREE.Geometry();
+
+reyForma.merge(cruzMalla.geometry, cruzMalla.matrix);
+reyForma.merge(cuerpoMalla.geometry, cuerpoMalla.matrix);
+reyForma.merge(baseMalla.geometry, baseMalla.matrix);
+
+var material = new THREE.MeshNormalMaterial();
+var reyMalla = new THREE.Mesh(reyForma, material);
+var escena = new THREE.Scene();
+escena.add(reyMalla);
+
+//------------------------------------------------
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
