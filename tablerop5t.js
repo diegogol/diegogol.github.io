e77de63acd185e113b744f2d7920896c71593559
diegogol <dicheck_km13@hotmail.com>
2017-03-20 21:02:36 -0600

    Update and rename tablerop8f.js to tablerop5t.js

--- tablerop5t.js
+++ tablerop5t.js
@@ -1,9 +1,9 @@
 var camara = new THREE.OrthographicCamera();
 
-camara.left = window.innerWidth/-4;
-camara.right = window.innerWidth/4;
-camara.top = window.innerHeight/4;
-camara.bottom= window.innerHeight/-4;
+camara.left = window.innerWidth/-5;
+camara.right = window.innerWidth/5;
+camara.top = window.innerHeight/5;
+camara.bottom= window.innerHeight/-5;
 camara.near = 1;
 camara.far = 45;
 camara.updateProjectionMatrix();
