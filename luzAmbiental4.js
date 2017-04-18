7ae85d39fa3fd25246f9c2b8ab2199dc4fbddd7a
diegogol <dicheck_km13@hotmail.com>
2017-04-18 13:49:13 -0500

    Update and rename luzAmbiental3.js to luzAmbiental4.js

--- luzAmbiental4.js
+++ luzAmbiental4.js
@@ -1,4 +1,4 @@
-var iluminacion = new THHREE.AmbientLight(0xFFFFFF);
+var iluminacion = new THREE.AmbientLight(0xFFFFFF);
 
 var forma= new THREE.SphereGeometry(1);
 var material =new THREE.MeshLambertMaterial({color: "#00cc00"});
