497551d06ca3d60e601d820df0f28ff95f152f2c
diegogol <dicheck_km13@hotmail.com>
2017-04-18 13:27:17 -0500

    Create luzAmbiental.js

---
+++ luzAmbiental.js
@@ -0,0 +1,20 @@
+var iluminacion = new THHREE.AmbientLight(0xFFFFFF);
+
+var forma= new THREE.SphereGeometry(1);
+var material =new THREE.MeshLambertMaterial({color: "#00cc00"});
+var malla =new THREE.Mesh(forma,material);
+
+var escena = New THREE.Scene();
+escena.add(malla);
+escena.add(iluminacion);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+var lienzo = document.getELementById("LuzAmbiental");
+var renderizador = new THREE.WebGLRenderer({canvas: lienzo,
+                                            antialias: true});
+                                            
+renderizador.setSize(600,600);
+document.body.appendChild( renderizador.domElement );
+renderizador.render(escena,camara);
