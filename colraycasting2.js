b5796796e16394c12dd5ea88cecf85cebbc1ef66
diegogol <dicheck_km13@hotmail.com>
2017-05-23 13:48:27 -0500

    Update and rename colraycasting.js to colraycasting2.js

--- colraycasting2.js
+++ colraycasting2.js
@@ -10,7 +10,7 @@ function setup(){
     camara.position.z = 5;
     
     raycaster = new THREE.Raycaster();
-    raycaster.setFromCamera( new THREE.Vector(0,0),camara);
+    raycaster.setFromCamera( new THREE.Vector2(0,0),camara);
     
     escena = new THREE.Scene();
     escena.add(cubo1);
