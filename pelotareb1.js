87707e699f7906a2bc1e3219173347ab24f50e3f
diegogol <dicheck_km13@hotmail.com>
2017-05-25 13:28:26 -0500

    Update and rename pelotarebota.js to pelotareb1.js

--- pelotareb1.js
+++ pelotareb1.js
@@ -1,4 +1,4 @@
-function setutp(){
+function setup(){
          cubo1  = new THREE.Mesh( new THREE.BoxGeometry(1,1,1),
                                   new THREE.MeshNormalMaterial());
          cubo2  = new THREE.Mesh( new THREE.BoxGeometry(1,1,1),
