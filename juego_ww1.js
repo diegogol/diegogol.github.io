12e51e04214c7e8372e79a115f127adaecee9011
diegogol <dicheck_km13@hotmail.com>
2017-05-30 13:56:55 -0500

    Update and rename juego_dd1.js to juego_ww1.js

--- juego_ww1.js
+++ juego_ww1.js
@@ -132,10 +132,10 @@ escena.add(cubo2);
 var campoVision     =45;//grados
 var relacionAspecto =window.innerWidth/window.innerHeight;
 var PlanoCercano    =1;
-var PlanoLejano     =1000;
+var PlanoLejano     =100;
 
 var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, PlanoCercano, PlanoLejano);
-camara.position.z=50;
+camara.position.z=100;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth, window.innerHeight);
