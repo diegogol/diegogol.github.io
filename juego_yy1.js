2566e5fa2bef052f0cbe0de852be352fdac31e8b
diegogol <dicheck_km13@hotmail.com>
2017-05-30 14:20:24 -0500

    Update and rename juego_uu1.js to juego_yy1.js

--- juego_yy1.js
+++ juego_yy1.js
@@ -135,7 +135,7 @@ var PlanoCercano    =10;
 var PlanoLejano     =1000;
 
 var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, PlanoCercano, PlanoLejano);
-camara.position.x=80;
+camara.position.y=50;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth, window.innerHeight);
