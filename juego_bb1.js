463a7012068c92d5e195ceab1e9f3ca52356fc24
diegogol <dicheck_km13@hotmail.com>
2017-05-30 15:24:34 -0500

    Update and rename juego_xx1.js to juego_bb1.js

--- juego_bb1.js
+++ juego_bb1.js
@@ -129,7 +129,7 @@ escena.add(reyMalla2);
 escena.add(cubo1);
 escena.add(cubo2);
 //------------------------------------------------
-var campoVision     =0;//grados
+var campoVision     =10;//grados
 var relacionAspecto =window.innerWidth/window.innerHeight;
 var PlanoCercano    =1;
 var PlanoLejano     =100;
